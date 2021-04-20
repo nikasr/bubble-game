5f89f07cf60bf55a449ab563750844e824e4d452
Monica <msrodrigues68@hotmail.com>
2021-04-20 12:35:23 -0300

    implementação da interceptação do click do mouse em cima da bolha e inclusão de um placar

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ var canvas = document.getElementById("buble"), context = canvas.getContext("2d")
 var amimacao = 900; // Tempo da animação (em milisegundos)
 var radius = 50; // Determina o raio do circulo
 var x = 0;
+var cont = 0; //contatdor do placar
+
+//coordenadas da bolha no momento do click
+var xbola = 0;
+var ybola = 0;
+
 
 // Monta o circulo no canvas com a posição
 function desenhaCirculo() {
@@ -21,6 +27,10 @@ function desenhaCirculo() {
     my_gradient.addColorStop(1, "white");
 
     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+
+    xbola = centerX;
+    ybola = centerY;
+
     context.fillStyle = my_gradient;
 
     context.fill();
@@ -29,36 +39,49 @@ function desenhaCirculo() {
 
     context.stroke();
 
+    Placar();
+
     // Adiciona uma cahamada a cada X milisegundos
     window.setTimeout(desenhaCirculo, amimacao);
 }
 
-//Conta as bolhas furadas
-function Furou() {
-    x++;
-   alert("furou " + x);
+function getCursorPosition(e) {
+    var x;
+    var y;
+    
+    //pega as coordenadas do click
+    if (e.pageX != undefined && e.pageY != undefined) {
+        x = e.pageX;
+        y = e.pageY;
+    }
+    else {
+        x = e.clientX + document.body.scrollLeft +
+                document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop +
+                document.documentElement.scrollTop;
+    }
+    
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+
+
+    if ((x >= (xbola-80)) && (x <= (xbola+80))) {
+        if ((y >= (ybola-80)) && (y <= (ybola+80))) {
+            cont++;
+        }
+    }
+    
+}
+
+function Placar(){
+ 
+	context.font = "bold 18px sans-serif";	
+	context.fillStyle = "red";
+	context.fillText('SCORE = '+ cont,480,20);
 }
 
 // Adiciona movimento ao carregar a página
 window.onload = desenhaCirculo;
+window.addEventListener("click", getCursorPosition, true);
 
 
-
-
-/*
-function iniciarJogo(){ 
-
-        context.beginPath();
-
-        context.arc(300, 200, 50, 0, 2*Math.PI);
-        context.fill();
-       // context.stroke(); 
-
-        context.moveTo(150,200);
-
-        context.arc(100, 200, 50, 0, 2*Math.PI);
-        context.stroke();         
-
-}
-let jogo = setInterval(iniciarJogo, 100);
-*/
